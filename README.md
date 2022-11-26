ec3257bdddba93a2e009339489ecc9e370467e28
Gwe2B <gwenael2b@live.fr>
2022-11-26 18:31:35 +0100

    Creating the accordion details
    Create the accordions to contain all datas concerning the approach of
    the NEOs and the orbital information.
    
    still todo: see why the SvgPicture class doesn't render the SVG.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 A school flutter project.
 Based on the NASA's NEO API.
+
+## Credits
+
+The icons come principaly from the default Icons library of flutter but there is some from tiers party:
+ - <a href="https://iconscout.com/icon-pack/planets-1" target="_blank">Planets Icon Pack</a> on <a href="https://iconscout.com">IconScout</a>
+ - <a href="https://iconscout.com/icons/orbit" target="_blank">Orbit Icon</a> on <a href="https://iconscout.com">IconScout</a>
+ - <a href="https://iconscout.com/icons/distance" target="_blank">Distance Icon</a> on <a href="https://iconscout.com">IconScout</a>
--- lib/detailed_view.dart
+++ lib/detailed_view.dart
@@ -1,4 +1,7 @@
+import 'package:accordion/accordion.dart';
+import 'package:accordion/controllers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'model/neo.dart';
 
@@ -11,6 +14,8 @@ class DetailedView extends StatefulWidget {
 }
 
 class _DetailedViewState extends State<DetailedView> {
+  static const double viewPadding = 8.0;
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -26,33 +31,70 @@ class _DetailedViewState extends State<DetailedView> {
     return Scaffold(
       appBar: AppBar(title: const Text('Near Earth Object')),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.object.name,
-              overflow: TextOverflow.clip,
-              style: textTheme.headlineLarge
-                  ?.copyWith(fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-            Text(
-              'id: ${widget.object.id}',
-              style: textTheme.bodyMedium?.copyWith(color: Colors.grey),
-            ),
-            RichText(
-              overflow: TextOverflow.clip,
-                text:
-                    TextSpan(text: 'Ø', style: textTheme.bodyLarge, children: [
-              TextSpan(
-                  text: 'Min. : ${minDiameter.toString()}km; ',
-                  style: textTheme.bodyMedium),
-              TextSpan(text: 'Ø', style: textTheme.bodyLarge),
-              TextSpan(
-                  text: 'Max. : ${maxDiameter.toString()}km',
-                  style: textTheme.bodyMedium)
-            ]))
-          ],
+        padding: const EdgeInsets.all(viewPadding),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width - (2 * viewPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.object.name,
+                maxLines: 2,
+                style: textTheme.headlineLarge?.copyWith(
+                    fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text(
+                'id: ${widget.object.id}',
+                style: textTheme.bodyMedium?.copyWith(color: Colors.grey),
+              ),
+              RichText(
+                  maxLines: 3,
+                  text: TextSpan(
+                      text: 'Ø',
+                      style: textTheme.bodyLarge,
+                      children: [
+                        TextSpan(
+                            text: 'Min. : ${minDiameter.toString()}km; ',
+                            style: textTheme.bodyMedium),
+                        TextSpan(text: 'Ø', style: textTheme.bodyLarge),
+                        TextSpan(
+                            text: 'Max. : ${maxDiameter.toString()}km',
+                            style: textTheme.bodyMedium),
+                      ])),
+              Accordion(
+                  maxOpenSections: 2,
+                  scaleWhenAnimating: true,
+                  openAndCloseAnimation: true,
+                  headerPadding:
+                      const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+                  sectionOpeningHapticFeedback: SectionHapticFeedback.heavy,
+                  sectionClosingHapticFeedback: SectionHapticFeedback.light,
+                  children: [
+                    AccordionSection(
+                      leftIcon: SvgPicture.asset('assets/orbit.svg'),
+                      header: const Text(
+                        'Orbital Informations',
+                        maxLines: 2,
+                      ),
+                      content: const Text('Lorem Ipsum Dolor Sin Amet'),
+                      contentHorizontalPadding: 20,
+                      contentBorderWidth: 1,
+                    ),
+                    AccordionSection(
+                      header: const Text(
+                        'Approach Datas',
+                        maxLines: 2,
+                      ),
+                      content: const Text('Lorem Ipsum Dolor Sin Amet'),
+                      contentHorizontalPadding: 20,
+                      contentBorderWidth: 1,
+                    )
+                  ]),
+                  Text('Gallery', style: textTheme.bodyLarge,),
+                  // Make a GridView
+                  const Text('Grid View Incomming')
+            ],
+          ),
         ),
       ),
     );
